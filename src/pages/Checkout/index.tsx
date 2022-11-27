afc895bbeb7c4b70fac0006c3ae66c53f134abf6
douglassantiagos <douglassantiagods@hotmail.com>
2022-11-27 09:53:15 -0300

    debugging to deploy

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -69,7 +69,7 @@ export const paymentMethods = {
 }
 
 export default function Checkout() {
-  const [cepFilled, setCepFilled] = useState('')
+  const [cepFilled, setCepFilled] = useState<string | any>('')
   const navigate = useNavigate()
   
   const { 
